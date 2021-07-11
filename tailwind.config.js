67a26681c2c132b246af16f2c4d7c97c09b92e8f
Lucas 洛可 <ycs77lucasv@gmail.com>
2021-07-11 23:14:52 +0800

    Install Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
